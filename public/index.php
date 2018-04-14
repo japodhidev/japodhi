9f4b1ca10872dd9bbc290ef47187f1177a84d979
japodhidev <japodhidev@gmail.com>
2018-04-14 15:28:13 +0300

    added prod envs

--- public/index.php
+++ public/index.php
@@ -12,7 +12,7 @@ if (!isset($_SERVER['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
         throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
     }
-    (new Dotenv())->load(__DIR__.'/../.env');
+    (new Dotenv())->load(__DIR__.'/../env');
 }
 
 $env = $_SERVER['APP_ENV'] ?? 'dev';
@@ -37,3 +37,4 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
