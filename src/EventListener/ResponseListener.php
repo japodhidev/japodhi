06a69a7a0ed8e790055e991d4479b705264a52f1
japodhidev <japodhidev@gmail.com>
2018-04-19 18:25:42 +0300

    security - response headers

--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -23,6 +23,9 @@ class ResponseListener
         $response->headers->set('X-Frame-Options', 'deny');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+        $response->headers->set('Content-Security-policy', "default-src 'self'; script-src https://static.ads-twitter.com https://www.google-analytics.com 'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='; img-src 'self' https://s3.amazonaws.com https://twitter.com https://pbs.twimg.com; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; frame-ancestors 'none';" );
+        $response->headers->set('Referrer-Policy', 'no-referrer, strict-origin-when-cross-origin');
+
 
 
         $response->prepare($event->getRequest());
