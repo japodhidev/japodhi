2146d27b62880eeef07a621f2322334f859cd89b
japodhidev <japodhidev@gmail.com>
2018-04-22 22:36:34 +0300

    final CSP response headers configs

--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -23,8 +23,9 @@ class ResponseListener
         $response->headers->set('X-Frame-Options', 'deny');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-        $response->headers->set('Content-Security-policy', "default-src 'self'; script-src https://static.ads-twitter.com https://www.google-analytics.com 'sha256-q2sY7jlDS4SrxBg6oq/NBYk9XVSwDsterXWpH99SAn0='; img-src 'self' https://s3.amazonaws.com https://twitter.com https://pbs.twimg.com; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; frame-ancestors 'none';" );
+        $response->headers->set('Content-Security-policy', "default-src 'self' *.japodhidev.com; script-src 'unsafe-inline' https://www.google-analytics.com  http://www.japodhidev.com https://maps.googleapis.com https://www.googletagmanager.com/gtag/js?id=UA-117919775-1 https://use.fontawesome.com http://maps.googleapis.com; img-src 'self' http://www.japodhidev.com  https://maps.gstatic.com https://maps.googleapis.com http://maps.googleapis.com; font-src 'self' http://www.japodhidev.com https://fonts.gstatic.com https://use.fontawesome.com http://maps.googleapis.com; style-src 'unsafe-inline' http://www.japodhidev.com https://fonts.googleapis.com https://maps.googleapis.com https://www.googletagmanager.com https://use.fontawesome.com http://maps.googleapis.com;" );
         $response->headers->set('Referrer-Policy', 'no-referrer, strict-origin-when-cross-origin');
+        $response->headers->set('Set-Cookie', 'secure');
 
 
 
