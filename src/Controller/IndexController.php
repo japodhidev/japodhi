6e77df4cbb2f97b5631439a9490c09d9ef44383c
japodhidev <japodhidev@gmail.com>
2019-11-22 14:38:25 +0300

    typos

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,13 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: finch
- * Date: 4/3/2018
- * Time: 7:48 PM
- */
 
 namespace App\Controller;
-
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
--- src/EventListener/ResponseListener.php
+++ src/EventListener/ResponseListener.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: finch
- * Date: 4/13/2018
- * Time: 7:10 PM
- */
 
 namespace App\EventListener;
 
@@ -17,7 +11,6 @@ class ResponseListener
 
     public function onKernelResponse(FilterResponseEvent $event) {
 
-
         $response = $event->getResponse();
         $response->headers->set('X-XSS-Protection', '1; mode=block');
         $response->headers->set('X-Frame-Options', 'deny');
@@ -26,8 +19,6 @@ class ResponseListener
         $response->headers->set('Content-Security-policy', "default-src 'self' *.japodhidev.com; script-src 'unsafe-inline' https://www.google-analytics.com  http://www.japodhidev.com https://maps.googleapis.com https://www.googletagmanager.com/gtag/js?id=UA-117919775-1 https://use.fontawesome.com http://maps.googleapis.com; img-src 'self' http://www.japodhidev.com  https://maps.gstatic.com https://maps.googleapis.com http://maps.googleapis.com; font-src 'self' http://www.japodhidev.com https://fonts.gstatic.com https://use.fontawesome.com http://maps.googleapis.com; style-src 'unsafe-inline' http://www.japodhidev.com https://fonts.googleapis.com https://maps.googleapis.com https://www.googletagmanager.com https://use.fontawesome.com http://maps.googleapis.com;" );
         $response->headers->set('Referrer-Policy', 'no-referrer, strict-origin-when-cross-origin');
         $response->headers->set('Set-Cookie', 'secure');
-
-
 
         $response->prepare($event->getRequest());
 
