50fe03f10d5b11bb71293b0529f5db721e970cf5
japodhidev <japodhidev@gmail.com>
2020-01-19 23:39:29 +0300

    package upgrades, maintenance

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,74 @@
 var Encore = require('@symfony/webpack-encore');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
-    // the project directory where compiled assets will be stored
+    // directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
+    // public path used by the web server to access the output path
     .setPublicPath('/build')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
 
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    /*
+     * ENTRY CONFIG
+     *
+     * Add 1 entry for each "page" of your app
+     * (including one that's included on every page - e.g. "app")
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app', './assets/js/app.js')
+    //.addEntry('page1', './assets/js/page1.js')
+    //.addEntry('page2', './assets/js/page2.js')
 
-    // uncomment to define the assets of the project
-    .addEntry('js/bootstrap', './assets/js/bootstrap.min.js')
-    .addEntry('js/google_map', './assets/js/google_map.js')
-    .addEntry('js/main', './assets/js/main.js')
-    .addEntry('js/jquery.countTo', './assets/js/jquery.countTo')
-    .addEntry('js/jquery.easing.1.3', './assets/js/jquery.easing.1.3')
-    .addEntry('js/jquery', './assets/js/jquery.magnific-popup.min.js')
-    .addEntry('js/jquery.min', './assets/js/jquery.min.js')
-    .addEntry('js/jquery.stellar.min', './assets/js/jquery.stellar.min')
-    .addEntry('js/jquery.style.switcher', './assets/js/jquery.style.switcher.js')
-    .addEntry('js/jquery.waypoints.min', './assets/js/jquery.waypoints.min.js')
-    .addEntry('js/magnific-popup-options', './assets/js/jquery-magnific-popup-options.js')
-    .addEntry('js/modernizr-2.6.2.min', './assets/js/modernizr-2.6.2.min.js')
-    .addEntry('js/respond.min', './assets/js/respond.min.js')
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
 
-    .addStyleEntry('css/animate', './assets/css/animate.css')
-    .addStyleEntry('css/icomoon', './assets/css/icomoon.css')
-    .addStyleEntry('css/simple-line-icons', './assets/css/simple-line-icons.css')
-    .addStyleEntry('css/magnific-popup', './assets/css/magnific-popup.css')
-    .addStyleEntry('css/bootstrap', './assets/css/bootstrap.css')
-    .addStyleEntry('css/pink', './assets/css/style3.css')
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
 
-
-    // uncomment if you use Sass/SCSS files
-    // allow sass/scss files to be processed
-    .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    .autoProvidejQuery()
-
-    .enableSourceMaps(!Encore.isProduction())
-
+    /*
+     * FEATURE CONFIG
+     *
+     * Enable & configure other features below. For a full
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
     .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
 
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    // enables Sass/SCSS support
+    //.enableSassLoader()
+
+    // uncomment if you use TypeScript
+    //.enableTypeScriptLoader()
+
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    //.enableIntegrityHashes(Encore.isProduction())
+
+    // uncomment if you're having problems with a jQuery plugin
+    //.autoProvidejQuery()
+
+    // uncomment if you use API Platform Admin (composer req api-admin)
+    //.enableReactPreset()
+    //.addEntry('admin', './assets/js/admin.js')
 ;
 
 module.exports = Encore.getWebpackConfig();
